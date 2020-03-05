6d8766f6c5fc260092b3c1eb0031a36ba7641532
Vitor George <vitor.george@gmail.com>
2020-03-05 13:24:13 +0000

    Add content to the side panel.

--- app/assets/scripts/components/home/SidePanel.js
+++ app/assets/scripts/components/home/SidePanel.js
@@ -9,7 +9,13 @@ export default class SidePanel extends Component {
   render () {
     return (
       <InpageBody>
-        <h1>Welcome!</h1>
+        <h1>About</h1>
+        <div>
+          Voluptate aliquip ullamco incididunt incididunt exercitation eu. Consectetur ullamco eiusmod id mollit cillum proident in adipisicing reprehenderit. Et dolor excepteur Lorem laboris adipisicing magna commodo. Est minim et nulla sint do magna elit aliquip officia eu ut culpa. Nulla nisi consectetur et officia reprehenderit dolor eiusmod laboris exercitation magna et sit. Qui labore amet eiusmod mollit culpa cupidatat laboris nostrud ad velit amet.
+        </div>
+        <div>
+          Read more about this project >
+        </div>        
       </InpageBody>
     );
   }
