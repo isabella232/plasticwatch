c03892e1f531b5d217a2b1d69298030d6dbbe1ab
Lane Goodman <Lane@developmentseed.org>
2020-03-26 23:13:12 -0400

    Initial trend page styling

--- app/assets/scripts/components/trends/index.js
+++ app/assets/scripts/components/trends/index.js
@@ -8,17 +8,17 @@ import { wrapApiResult, getFromState } from '../../redux/utils';
 import * as actions from '../../redux/actions/places';
 
 import App from '../common/app';
-import { Inpage, InpageBody } from '../common/inpage';
 import { InnerPanel, Panel } from '../../styles/panel';
 import Button from '../../styles/button/button';
 import { themeVal } from '../../styles/utils/general';
 import DataTable from '../../styles/table';
 import { round } from '../../utils/utils';
+import media from '../../styles/utils/media-queries';
 
 const PanelStats = styled.div`
   display: flex;
-  flex-flow: row wrap;
-  margin: 2rem 0;
+  flex-flow: row nowrap;
+  margin: 2rem auto;
 `;
 
 const PanelStat = styled.h2`
@@ -31,10 +31,26 @@ const PanelStat = styled.h2`
   }
   span {
     color: ${themeVal('color.baseLight')};
-    font-size: 0.875rem;
+    font-size: 0.75rem;
     text-transform: uppercase;
-    word-break: break-all;
   }
+`;
+
+const TwoPanelLayout = styled(Panel)`
+  ${InnerPanel} {
+    margin: 0;
+    justify-content: flex-start;
+    height: calc(100vh - 8rem);
+    &:not(:last-of-type) {
+      margin-bottom: 2rem;
+    }
+  }
+  ${media.mediumUp`
+    display: grid;
+    grid-template-columns: 1fr 2fr;
+    grid-gap: 2rem;
+    height: 100vh;
+  `};
 `;
 
 class Trends extends React.Component {
@@ -57,113 +73,109 @@ class Trends extends React.Component {
 
     return (
       <App pageTitle='Trends'>
-        <Inpage>
-          <InpageBody>
-            <Panel>
-              <InnerPanel>
-                <h2>Washington DC</h2>
-                <p>{percentSurveyed}% restaurants surveyed</p>
-                <p>
-                  {surveyedPlacesCount} of {placesCount} restaurants on
-                  OpenStreetMap
-                </p>
-                <h3>XX%</h3>
-                <p>
-                  XXXX Surveyed Washington DC Restaurants offer plastic-free
-                  options
-                </p>
-                <PanelStats>
-                  <PanelStat>
-                    {surveyedPlacesCount}
-                    <span>
-                      Restaurants
-                      <br />
-                      Surveyed
-                    </span>
-                  </PanelStat>
-                  <PanelStat>
-                    {surveyorsCount}
-                    <span>Surveyors</span>
-                  </PanelStat>
-                  <PanelStat>
-                    {placesCount - surveyedPlacesCount}
-                    <span>
-                      Restaurants to
-                      <br />
-                      Survey
-                    </span>
-                  </PanelStat>
-                </PanelStats>
-                <Button useIcon='map' variation='base-raised-dark'>
-                  Show me the map
-                </Button>
-              </InnerPanel>
-              <InnerPanel>
-                <h2>Top Surveyors</h2>
-                <DataTable>
-                  <thead>
-                    <tr>
-                      <th>Rank</th>
-                      <th>Surveyor</th>
-                      <th>Surveys</th>
-                      <th>Action</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <tr>
-                      <td>1</td>
-                      <td>Bob Smith</td>
-                      <td>24</td>
-                      <td>
-                        <Button
-                          size='small'
-                          variation='base-plain'
-                          useIcon='trash-bin'
-                          hideText
-                          title='Remove user'
-                        >
-                          Remove
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>2</td>
-                      <td>Jane Good</td>
-                      <td>19</td>
-                      <td>
-                        <Button
-                          size='small'
-                          variation='base-plain'
-                          useIcon='trash-bin'
-                          hideText
-                          title='Remove user'
-                        >
-                          Remove
-                        </Button>
-                      </td>
-                    </tr>
-                    <tr>
-                      <td>3</td>
-                      <td>Matt Park</td>
-                      <td>12</td>
-                      <td>
-                        <Button
-                          size='small'
-                          variation='base-plain'
-                          useIcon='trash-bin'
-                          hideText
-                          title='Remove user'
-                        >
-                          Remove
-                        </Button>
-                      </td>
-                    </tr>
-                  </tbody>
-                </DataTable>
-              </InnerPanel>
-            </Panel>
-          </InpageBody>
-        </Inpage>
+        <TwoPanelLayout>
+          <InnerPanel>
+            <h2>Washington DC</h2>
+            <p>{percentSurveyed}% restaurants surveyed</p>
+            <p>
+              {surveyedPlacesCount} of {placesCount} restaurants on
+              OpenStreetMap
+            </p>
+            <h3>XX%</h3>
+            <p>
+              XXXX Surveyed Washington DC Restaurants offer plastic-free
+              options
+            </p>
+            <PanelStats>
+              <PanelStat>
+                {surveyedPlacesCount}
+                <span>
+                  Restaurants
+                  <br />
+                  Surveyed
+                </span>
+              </PanelStat>
+              <PanelStat>
+                {surveyorsCount}
+                <span>Surveyors</span>
+              </PanelStat>
+              <PanelStat>
+                {placesCount - surveyedPlacesCount}
+                <span>
+                  Restaurants to
+                  <br />
+                  Survey
+                </span>
+              </PanelStat>
+            </PanelStats>
+            <Button useIcon='map' variation='base-raised-dark'>
+              Show me the map
+            </Button>
+          </InnerPanel>
+          <InnerPanel>
+            <h2>Top Surveyors</h2>
+            <DataTable>
+              <thead>
+                <tr>
+                  <th>Rank</th>
+                  <th>Surveyor</th>
+                  <th>Surveys</th>
+                  <th>Action</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>1</td>
+                  <td>Bob Smith</td>
+                  <td>24</td>
+                  <td>
+                    <Button
+                      size='small'
+                      variation='base-plain'
+                      useIcon='trash-bin'
+                      hideText
+                      title='Remove user'
+                    >
+                      Remove
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>2</td>
+                  <td>Jane Good</td>
+                  <td>19</td>
+                  <td>
+                    <Button
+                      size='small'
+                      variation='base-plain'
+                      useIcon='trash-bin'
+                      hideText
+                      title='Remove user'
+                    >
+                      Remove
+                    </Button>
+                  </td>
+                </tr>
+                <tr>
+                  <td>3</td>
+                  <td>Matt Park</td>
+                  <td>12</td>
+                  <td>
+                    <Button
+                      size='small'
+                      variation='base-plain'
+                      useIcon='trash-bin'
+                      hideText
+                      title='Remove user'
+                    >
+                      Remove
+                    </Button>
+                  </td>
+                </tr>
+              </tbody>
+            </DataTable>
+          </InnerPanel>
+        </TwoPanelLayout>
       </App>
     );
   }
