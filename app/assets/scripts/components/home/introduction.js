069f4895f52c8702e03b74f6448ba0985ea2e89f
Lane Goodman <Lane@developmentseed.org>
2020-04-02 15:46:30 -0400

    Add desktop background image, logo and flourish

--- app/assets/scripts/components/home/introduction.js
+++ app/assets/scripts/components/home/introduction.js
@@ -16,13 +16,33 @@ import { themeVal } from '../../styles/utils/general';
 
 const IntroPanel = styled(InnerPanel)`
   margin: 0;
-  padding-bottom: 2rem;
+  padding: 2rem;
   align-items: flex-start;
   justify-content: flex-end;
   grid-row: 2/5;
   z-index: 10;
+  border-radius: 0.5rem;
+  border: 2px solid ${themeVal('color.shadow')};
+  box-shadow: 0 -2px 12px 1px ${themeVal('color.shadow')};
   ${media.mediumUp`
     padding-bottom: 4rem;
+    background: ${themeVal('color.background')};
+    &:before {
+      content: '';
+      width: 100%;
+      height: 400px;
+      background: linear-gradient(150deg, transparent, #EEF6FC 50%), url('../../../assets/graphics/content/underwater.jpg');
+      position: absolute;
+      top: -10%;
+      left: 0;
+      z-index: -1;
+      clip-path: polygon(
+        0 0, /* left top */
+        100% 0, /* right top */ 
+        100% 20%, /* right bottom */
+        0 100% /* left bottom */
+      );   
+    }
   `};
   & > ${Button} {
     margin: 1rem 0 2rem;
@@ -30,6 +50,7 @@ const IntroPanel = styled(InnerPanel)`
 `;
 
 const IntroHeading = styled(Heading)`
+  margin: 0;
   img {
     display: block;
     max-width: 16rem;
@@ -38,6 +59,17 @@ const IntroHeading = styled(Heading)`
     font-weight: ${themeVal('type.heading.regular')};
   }
 `;
+
+const IntroSubHeading = styled(Heading)`
+  &:after {
+    content: '';
+    width: 16rem;
+    display: block;
+    margin: 0.25rem 0;
+    border: 0.25rem solid ${themeVal('color.primary')};
+  }
+`;
+
 const ModalButton = styled(Button)`
   text-transform: none;
   letter-spacing: 0;
@@ -116,6 +148,9 @@ export default class Introduction extends Component {
         <IntroHeading size='x-large' variation='primary'>
           <img src='../../../assets/graphics/content/Oceana_2.svg' alt='Oceana logo' /><span>Plastic</span>Watch
         </IntroHeading>
+        <IntroSubHeading>
+          About
+        </IntroSubHeading>
         <p><a href='https://oceana.org'>Oceana’s</a> PlasticWatch provides a platform for customers to survey restaurants’ single-use plastic usage and locate restaurants that offer plastic-free choices. Using crowdsourced ratings and reviews, PlasticWatch connects people who are seeking to reduce their plastic footprints with restaurants that provide plastic-free alternatives.
         </p>
         <ModalButton
@@ -145,7 +180,7 @@ export default class Introduction extends Component {
           onCloseClick={this.onModalClick.bind(this, false)}
           headerComponent={(
             <ModalHeader>
-              <Heading>About PlasticWatch</Heading>
+              <IntroSubHeading>About PlasticWatch</IntroSubHeading>
             </ModalHeader>
           )}
           bodyComponent={(
