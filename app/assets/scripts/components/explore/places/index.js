1f214f781fb44defafb69aa40eec9acc1df35d83
Sajjad Anwar <sajjadkm@gmail.com>
2020-04-09 17:03:13 +0530

    search in the list

--- app/assets/scripts/components/explore/places/index.js
+++ app/assets/scripts/components/explore/places/index.js
@@ -106,7 +106,7 @@ class PlacesIndex extends Component {
           <FilterToolbar>
             <InputWrapper>
               <FilterLabel htmlFor='placeSearch'>
-                Search in list
+                Search in the list
               </FilterLabel>
               <InputWithIcon
                 type='text'
