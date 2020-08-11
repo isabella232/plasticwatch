62693a3605e6c7ea826f01d3154fa2f4ec031f47
Vitor George <vitor.george@gmail.com>
2020-08-11 12:43:04 +0100

    Enable HTTPS on staging

--- app/assets/scripts/config/staging.js
+++ app/assets/scripts/config/staging.js
@@ -3,5 +3,5 @@
 module.exports = {
   environment: 'staging',
   baseUrl: 'https://plasticwatch.surge.sh',
-  apiUrl: 'http://wikiplastic-dev.us-east-1.elasticbeanstalk.com'
+  apiUrl: 'https://plasticwatch.ds.io'
 };
