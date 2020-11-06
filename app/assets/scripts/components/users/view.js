af1ff461990128fd1078b170f9eeec2ec2b8e2a7
Lane Goodman <Lane@developmentseed.org>
2020-11-06 16:14:36 -0500

    Style user profile
    Makes panelstat global style, adds user location and badge description

--- app/assets/scripts/components/users/view.js
+++ app/assets/scripts/components/users/view.js
@@ -3,9 +3,11 @@ import styled from 'styled-components';
 import { connect } from 'react-redux';
 import { environment } from '../../config';
 import media from '../../styles/utils/media-queries';
+import { themeVal } from '../../styles/utils/general';
+import { ScrollWrap } from '../../styles/table';
 
 import App from '../common/app';
-import { InnerPanel, Panel } from '../../styles/panel';
+import { InnerPanel, Panel, PanelStats, PanelStat } from '../../styles/panel';
 
 const TwoPanelLayout = styled(Panel)`
   ${InnerPanel} {
@@ -31,9 +33,80 @@ const TwoPanelLayout = styled(Panel)`
   `};
 `;
 
+const UserInfo = styled.div`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: center;
+  ${media.mediumUp`
+    flex-flow: row wrap;
+  `}
+`;
+
+const UserName = styled.h2`
+  flex-basis: 100%;
+  text-align: center;
+  ${media.mediumUp`
+    text-align: left;
+  `}
+`;
+
+const Avatar = styled.img`
+  margin-right: 1rem;
+  border-radius: 50%;
+  max-width: 150px;
+  border: 0.5rem solid ${themeVal('color.shadow')};
+`;
+
+const UserData = styled.div`
+  display: flex;
+  flex-flow: row nowrap;
+  flex: 1;
+  ${media.mediumUp`
+    flex-flow: column nowrap;
+  `}
+  ${PanelStat} {
+    border-right: none;
+  }
+`;
+
+const BadgeGrid = styled.ul`
+  display: grid;
+  margin: 1rem 0;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(225px, 1fr));
+  list-style: none;
+`;
+
+const BadgeItem = styled.li`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  ${media.mediumUp`
+    align-items: flex-start;
+  `}
+`;
+
+const BadgeName = styled.p`
+  font-size: 0.875rem;
+  font-weight: ${themeVal('type.base.regular')};
+  color: ${themeVal('color.secondary')};
+  letter-spacing: 0.25px;
+  text-transform: uppercase;
+  vertical-align: middle;
+  position: relative;
+  text-align: left;
+  margin: 0.5rem 0;
+`;
+
+const BadgeImg = styled.img`
+  border-radius: 50%;
+  border: 0.125rem solid ${themeVal('color.primary')};
+`;
+
 function Users() {
   const user = {
     name: 'Juliana Castillo',
+    location: 'Washington DC',
     lastSurvey: '10/10/20',
     surveyCount: 18,
     plasticFreeSurveyCount: 7,
@@ -46,32 +119,38 @@ function Users() {
     {
       id: 'badge-1',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 1'
+      title: 'Badge 1',
+      description: 'This badge is earned by mapping plastic-free locations'
     },
     {
       id: 'badge-2',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 2'
+      title: 'Badge 2',
+      description: 'This badge is earned by mapping plastic-free locations'
     },
     {
       id: 'badge-3',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 3'
+      title: 'Badge 3',
+      description: 'This badge is earned by mapping plastic-free locations'
     },
     {
       id: 'badge-4',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 4'
+      title: 'Badge 4',
+      description: 'This badge is earned by mapping plastic-free locations'
     },
     {
       id: 'badge-5',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 5'
+      title: 'Badge 5',
+      description: 'This badge is earned by mapping plastic-free locations'
     },
     {
       id: 'badge-6',
       image: 'https://via.placeholder.com/150/FF0000/FFFFFF',
-      title: 'Badge 6'
+      title: 'Badge 6',
+      description: 'This badge is earned by mapping plastic-free locations'
     }
   ];
 
@@ -79,29 +158,49 @@ function Users() {
     <App pageTitle='Users'>
       <TwoPanelLayout>
         <InnerPanel>
-          <h2>{user.name}</h2>
-          <div>
-            <img src={user.profilePic} />
-            <h3>City</h3>
-            <h3>Last Survey</h3>
-            <p>{user.lastSurvey}</p>
-          </div>
-          <div>
-            <p>{user.surveyCount} surveys</p>
-            <p>{user.plasticFreeSurveyCount} plastic free surveys</p>
-            <p>{user.campaignsCount} campaign contributed</p>
-          </div>
+          <UserInfo>
+            <UserName>{user.name}</UserName>
+            <Avatar src={user.profilePic} />
+            <UserData>
+              <PanelStat>
+                <span>City</span>
+                {user.location}
+              </PanelStat>
+              <PanelStat>
+                <span>Last Survey</span>
+                {user.lastSurvey}
+              </PanelStat>
+            </UserData>
+          </UserInfo>
+          <PanelStats>
+            <PanelStat>
+              {user.surveyCount}
+              <span>surveys</span>
+            </PanelStat>
+            <PanelStat>
+              {user.plasticFreeSurveyCount}
+              <span>plastic free surveys</span>
+            </PanelStat>
+            <PanelStat>
+              {user.campaignsCount}
+              <span>campaign contributed</span>
+            </PanelStat>
+          </PanelStats>
         </InnerPanel>
         <InnerPanel>
-          <h2>Badges earned</h2>
-          <ul>
-            {badges.map((b) => (
-              <li key={b.id}>
-                <img src={b.image} />
-                <span>{b.title}</span>
-              </li>
-            ))}
-          </ul>
+          <h2>Badges</h2>
+          <ScrollWrap>
+            <h4>EARNED</h4>
+            <BadgeGrid>
+              {badges.map((b) => (
+                <BadgeItem key={b.id}>
+                  <BadgeImg src={b.image} />
+                  <BadgeName>{b.title}</BadgeName>
+                  <p>{b.description}</p>
+                </BadgeItem>
+              ))}
+            </BadgeGrid>
+          </ScrollWrap>
         </InnerPanel>
       </TwoPanelLayout>
     </App>
