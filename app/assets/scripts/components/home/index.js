38fbac20b3f8d7241174af5b7965ad641604d54f
Lane Goodman <Lane@developmentseed.org>
2020-11-10 15:51:14 -0500

    Add campaign picker to home page

--- app/assets/scripts/components/home/index.js
+++ app/assets/scripts/components/home/index.js
@@ -1,14 +1,24 @@
 /* eslint-disable react/no-access-state-in-setstate */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { PropTypes as T } from 'prop-types';
+import { withRouter } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import App from '../common/app';
 import styled from 'styled-components';
-import { appPathname } from '../../config';
+import { environment, appPathname } from '../../config';
 import media from '../../styles/utils/media-queries';
 
+import * as exploreActions from '../../redux/actions/explore';
+import { wrapApiResult } from '../../redux/utils';
+import { fetchCampaigns } from '../../redux/actions/campaigns';
+
+import UhOh from '../uhoh';
 import { SidebarWrapper } from '../common/view-wrappers';
 import Introduction from './introduction';
 import { StyledLink } from '../common/link';
+import { Modal, ModalHeader, ModalBody } from '../common/modal';
+import Button from '../../styles/button/button';
 
 const HomeWrapper = styled(SidebarWrapper)`
   height: inherit;
@@ -39,17 +49,114 @@ const FakeMap = styled.div`
   }
 `;
 
-export default class Home extends React.Component {
-  render () {
+const CampaignList = styled.ul`
+  display: flex;
+  flex-flow: column nowrap;
+  align-items: stretch;
+  justify-content: space-around;
+  ${Button} {
+    text-decoration: none;
+    width: 100%;
+    margin-bottom: 1rem;
+  }
+`;
+
+function Home(props) {
+  const { campaigns } = props;
+  const {
+    params: { campaignSlug }
+  } = props.match;
+
+  const [showCampaignSelector, setShowCampaignSelector] = useState(false);
+
+  // Load campaigns on mount
+  useEffect(() => {
+    props.fetchCampaigns();
+  }, []);
+
+  // Helper function to handle clicks on nav tabs
+  function handleMapClick() {
+    // Show Campaign Selector
+    if (!campaignSlug) {
+      setShowCampaignSelector(true);
+    }
+  }
+
+  // Helper function to list campaigns in Modal
+  function renderCampaigns() {
+    if (!campaigns.isReady()) {
+      return <div>Loading campaigns...</div>;
+    }
+
+    if (campaigns.hasError()) {
+      return <UhOh />;
+    }
+
+    const allCampaigns = campaigns.getData();
+
+    if (allCampaigns.length === 0) {
+      return <div>No campaigns are available.</div>;
+    }
+
     return (
-      <App pageTitle='Welcome' hideFooter>
-        <HomeWrapper>
-          <Introduction />
-          <FakeMap as={StyledLink} to='/explore'>
-            <h4>Click the map to start exploring</h4>
-          </FakeMap>
-        </HomeWrapper>
-      </App>
+      <CampaignList>
+        {Object.keys(allCampaigns).map((cSlug) => {
+          const c = allCampaigns[cSlug];
+          return (
+            <li key={cSlug}>
+              <Button
+                as={StyledLink}
+                variation='primary-raised-light'
+                to={`/explore/${c.slug}`}
+                data-tip={`Go to ${c.name} campaign`}
+              >
+                {c.name}
+              </Button>
+            </li>
+          );
+        })}
+      </CampaignList>
     );
   }
+  return (
+    <App pageTitle='Welcome' hideFooter>
+      <HomeWrapper>
+        <Introduction />
+        <Modal
+          id='introExpanded'
+          revealed={showCampaignSelector}
+          onCloseClick={() => setShowCampaignSelector(false)}
+          headerComponent={<ModalHeader>Select a city</ModalHeader>}
+          bodyComponent={<ModalBody>{renderCampaigns()}</ModalBody>}
+        />
+        <FakeMap as={StyledLink} onClick={() => handleMapClick()}>
+          <h4>Click the map to start exploring</h4>
+        </FakeMap>
+      </HomeWrapper>
+    </App>
+  );
 }
+
+if (environment !== 'production') {
+  Home.propTypes = {
+    campaigns: T.object,
+    fetchCampaigns: T.func,
+    match: T.object
+  };
+}
+
+function mapStateToProps(state) {
+  return {
+    campaigns: wrapApiResult(state.campaigns)
+  };
+}
+
+function dispatcher(dispatch) {
+  return {
+    fetchCampaigns: (...args) => dispatch(fetchCampaigns(...args)),
+    updateActiveMobileTab: (...args) =>
+      dispatch(exploreActions.updateActiveMobileTab(...args))
+  };
+}
+
+export default connect(mapStateToProps, dispatcher)(withRouter(Home));
