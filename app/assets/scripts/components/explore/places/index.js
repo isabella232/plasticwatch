093e4391d5030798d86b5e53d14e97761d8435a3
Sajjad Anwar <sajjadkm@gmail.com>
2020-11-10 19:40:55 +0530

    lint

--- app/assets/scripts/components/explore/places/index.js
+++ app/assets/scripts/components/explore/places/index.js
@@ -74,7 +74,7 @@ class PlacesIndex extends Component {
     const { lat, lng, zoom } = this.props.mapViewport;
     const link = `https://openstreetmap.org/edit?editor=id&lat=${lat}&lon=${lng}&zoom=${zoom}`
     if (count === 0) {
-      return <p>No results found. May be you would like to <a target='_blank' href={link}>add a place to OpenStreetMap.</a></p>;
+      return <p>No results found. May be you would like to <a target='_blank' rel='noopener noreferrer' href={link}>add a place to OpenStreetMap.</a></p>;
     } else if (count.length === 1) return <div>1 place found in the area.</div>;
     else return <div>{count} places found in the area.</div>;
   }
