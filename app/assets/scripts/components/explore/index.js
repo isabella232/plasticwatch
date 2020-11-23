e08493fd7a43839d0de3d37fabc357e585af4cea
Vitor George <vitor.george@gmail.com>
2020-11-23 11:17:48 +0000

    Fix page title

--- app/assets/scripts/components/explore/index.js
+++ app/assets/scripts/components/explore/index.js
@@ -221,7 +221,7 @@ class Explore extends React.Component {
     if (isMobile && activeMobileTab !== 'map') displayMap = false;
 
     return (
-      <App pageTitle='About' hideFooter>
+      <App pageTitle='Explore' hideFooter>
         <SidebarWrapper>
           {isMobile && this.renderCampaignSelector(displayMap)}
           <Route exact path='/explore/:campaignSlug' component={PlacesIndex} />
